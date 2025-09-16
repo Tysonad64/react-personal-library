2b73dfa2475281bcd23c39dbcc5f26c7e48990b3
Tyson <tysonad3026@outlook.com>
2025-09-15 21:16:34 -0700

    added folder structure
    made locations for different types of documents

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Test</h1>
-      </div>
+      
     </>
   )
 }
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,6 +1,7 @@
 import './Greeting.css';
 
 function Greeting() {
+    
     return (
         <h1>Welcome to Dark Matter Books HQ</h1>
     )
