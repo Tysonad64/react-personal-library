b9a93503c36bfe13de2e7bce9e8a49881c2d602e
Tyson <tysonad3026@outlook.com>
2025-09-19 15:58:06 -0700

    Added new database connection
    PostgreSQL: DB: dmbhqDB

---
+++ Postgres.session.sql
@@ -0,0 +1,11 @@
+{
+  "ssh": "Disabled",
+  "previewLimit": 50,
+  "server": "localhost",
+  "port": 5433,
+  "askForPassword": true,
+  "driver": "PostgreSQL",
+  "database": "dmbhqDB",
+  "name": "Postgres",
+  "username": "postgres"
+}
